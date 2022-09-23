184ec7f88930592afb8faad650a279064bb15b1f
veyseltosun <postalarim1923@gmail.com>
2022-09-23 23:51:19 +0300

    commit

---
+++ tabs/src/App.js
@@ -0,0 +1,17 @@
+import{useState, useEffect} from  "react";
+import {FaAngleDoubleRight} from "react-icons/fa";
+
+const url = "https://course-api.com/react-tabs-project"
+
+function App() {
+
+
+  return (
+    <div className="App">
+      Job exp app 
+    </div>
+  );
+}
+
+export default App;
+rea
