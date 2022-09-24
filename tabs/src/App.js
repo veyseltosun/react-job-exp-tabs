b3f7ec0de5eef7370fa7dd3d323e59757fb7d97e
veyseltosun <postalarim1923@gmail.com>
2022-09-24 14:54:20 +0300

    app js

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -5,6 +5,26 @@ const url = "https://course-api.com/react-tabs-project"
 
 function App() {
 
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    fetch(url)
+    .then(response => response.json())
+    .then(response => setJobs(response))
+    setLoading(false)
+   
+  }, [])
+  console.log(jobs)
+  console.log(loading)
+  if (loading){
+    return(
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
 
   return (
     <div className="App">
@@ -14,4 +34,3 @@ function App() {
 }
 
 export default App;
-rea
